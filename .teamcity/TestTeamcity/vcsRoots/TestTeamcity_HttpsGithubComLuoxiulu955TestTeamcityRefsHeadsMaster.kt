13eaecf120db350688840307722efda399f66dd8
Liang Xia <rey.xia@gmail.com>
2018-03-05 15:46:48 -0800

    update vcsRoot

--- .teamcity/TestTeamcity/vcsRoots/TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster.kt
+++ .teamcity/TestTeamcity/vcsRoots/TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster.kt
@@ -5,7 +5,7 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.vcs.GitVcsRoot
 
 object TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster : GitVcsRoot({
     uuid = "b3f6e641-899f-4f27-bea3-dd0967939bb9"
-    id = "TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsTest-v1"
+    id = "TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsTest_v1"
     name = "https://github.com/luoxiulu955/TestTeamcity#refs/heads/test-v1"
     url = "https://github.com/luoxiulu955/TestTeamcity"
     authMethod = password {
