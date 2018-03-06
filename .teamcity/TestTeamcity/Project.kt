aa7a1940fb9180b9799f2f78fc7a601105526929
Liang Xia <rey.xia@gmail.com>
2018-03-05 17:02:37 -0800

    change config

--- .teamcity/TestTeamcity/Project.kt
+++ .teamcity/TestTeamcity/Project.kt
@@ -2,8 +2,11 @@ package TestTeamcity
 
 import TestTeamcity.buildTypes.*
 import TestTeamcity.vcsRoots.*
+import TestTeamcity.vcsRoots.TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster
 import jetbrains.buildServer.configs.kotlin.v2017_2.*
 import jetbrains.buildServer.configs.kotlin.v2017_2.Project
+import jetbrains.buildServer.configs.kotlin.v2017_2.projectFeatures.VersionedSettings
+import jetbrains.buildServer.configs.kotlin.v2017_2.projectFeatures.versionedSettings
 
 object Project : Project({
     uuid = "3ad4904a-bb68-49ec-b8da-951b6182599d"
@@ -14,5 +17,17 @@ object Project : Project({
     vcsRoot(TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster)
     vcsRoot(TestTeamcity_Git)
 
-    buildType(TestTeamcity_Build)
+    buildType(TestTeamcity_Testv1_Build)
+
+    features {
+        versionedSettings {
+            id = "PROJECT_EXT_2"
+            mode = VersionedSettings.Mode.ENABLED
+            buildSettingsMode = VersionedSettings.BuildSettingsMode.PREFER_SETTINGS_FROM_VCS
+            rootExtId = TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster.id
+            showChanges = true
+            settingsFormat = VersionedSettings.Format.KOTLIN
+            storeSecureParamsOutsideOfVcs = true
+        }
+    }
 })
--- .teamcity/TestTeamcity/vcsRoots/TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster.kt
+++ .teamcity/TestTeamcity/vcsRoots/TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster.kt
@@ -4,12 +4,12 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.*
 import jetbrains.buildServer.configs.kotlin.v2017_2.vcs.GitVcsRoot
 
 object TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster : GitVcsRoot({
-    uuid = "b3f6e641-899f-4f27-bea3-dd0967939bb9"
-    id = "TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsTest_v1"
-    name = "https://github.com/luoxiulu955/TestTeamcity#refs/heads/test-v1"
+    uuid = "b3f6e641-899f-4f27-bea3-dd0967939bb8"
+    id = "TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster"
+    name = "https://github.com/luoxiulu955/TestTeamcity#refs/heads/master"
     url = "https://github.com/luoxiulu955/TestTeamcity"
     authMethod = password {
         userName = "luoxiulu955"
-        password = "credentialsJSON:c1e97dd6-7ae8-4a80-9d4e-b0031923d25b"
+        password = "credentialsJSON:d276e0b0-57a2-4f31-a883-6e2ee2c7f56d"
     }
 })
