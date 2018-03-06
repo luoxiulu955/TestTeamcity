3f688a0454ff1d3b240596286b0a6cf0bbaa5b00
lxia <lxia@TeamCity>
2018-03-06 10:44:49 -0800

    TeamCity change in 'TestTeamcity' project: Versioned settings configuration updated

---
+++ .teamcity/TestTeamcity/Project.kt
@@ -0,0 +1,32 @@
+package TestTeamcity
+
+import TestTeamcity.buildTypes.*
+import TestTeamcity.vcsRoots.*
+import TestTeamcity.vcsRoots.TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster
+import jetbrains.buildServer.configs.kotlin.v2017_2.*
+import jetbrains.buildServer.configs.kotlin.v2017_2.Project
+import jetbrains.buildServer.configs.kotlin.v2017_2.projectFeatures.VersionedSettings
+import jetbrains.buildServer.configs.kotlin.v2017_2.projectFeatures.versionedSettings
+
+object Project : Project({
+    uuid = "3ad4904a-bb68-49ec-b8da-951b6182599d"
+    id = "TestTeamcity"
+    parentId = "_Root"
+    name = "TestTeamcity"
+
+    vcsRoot(TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster)
+
+    buildType(TestTeamcity_Build)
+
+    features {
+        versionedSettings {
+            id = "PROJECT_EXT_2"
+            mode = VersionedSettings.Mode.ENABLED
+            buildSettingsMode = VersionedSettings.BuildSettingsMode.PREFER_SETTINGS_FROM_VCS
+            rootExtId = TestTeamcity_HttpsGithubComLuoxiulu955TestTeamcityRefsHeadsMaster.id
+            showChanges = false
+            settingsFormat = VersionedSettings.Format.KOTLIN
+            storeSecureParamsOutsideOfVcs = true
+        }
+    }
+})
